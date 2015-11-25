9fcacd776ebed413c93b47558dfd239f31431004
stayForward09 <daveescott0059@gmail.com>
2015-11-24 22:53:31 -0800

    Removing router logic to separate component.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,16 +1,6 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
 
-var Router = require('react-router').Router;
-var IndexRoute = require('react-router').IndexRoute;
-var Route = require('react-router').Route;
-var history = require('history').createHistory();
-
-var Home = require('./home.jsx');
-var Page = require('./page.jsx');
-
-
-var App = React.createClass({
+module.exports = React.createClass({
 
     propTypes: {
         children: React.PropTypes.oneOfType([React.PropTypes.array, React.PropTypes.element])
@@ -28,13 +18,3 @@ var App = React.createClass({
         );
     }
 });
-
-
-ReactDOM.render((
-    <Router history={history}>
-	    <Route path="/" component={App}>
-	        <IndexRoute component={Home} />
-	    </Route>
-	    <Route path="/page" component={Page} />
-	</Router>
-), document.getElementById('appContainer'));
