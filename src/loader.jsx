96feb1e7250505192e284799ea5a1c96fa13692b
stayForward09 <daveescott0059@gmail.com>
2015-11-24 22:52:41 -0800

    Adding main jsx loader file for app.

---
+++ src/loader.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Router = require('react-router').Router;
+var IndexRoute = require('react-router').IndexRoute;
+var Route = require('react-router').Route;
+var history = require('history').createHistory();
+
+var Home = require('./home.jsx');
+var Page = require('./page.jsx');
+var App = require('./components/app.jsx');
+
+ReactDOM.render((
+    <Router history={history}>
+	    <Route path="/" component={App}>
+	        <IndexRoute component={Home} />
+	    </Route>
+	    <Route path="/page" component={Page} />
+	</Router>
+), document.getElementById('appContainer'));
